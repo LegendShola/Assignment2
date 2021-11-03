c8a76baa3fcfa5c81529e3fb33b3719ecf8eb32b
shola.olagbemisoye <shola.olagbemisoye@NGKW0204LT1>
2021-11-03 08:53:53 +0100

    Add project files.

---
+++ Assignment2/Models/Customer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment2.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Bvn { get; set; }
+
+
+        public void Login()
+        {
+            Console.WriteLine("Pls Enter Your UserName");
+            UserName = Console.ReadLine();
+
+            if (UserName == "Shola")
+            {
+                Console.WriteLine("Login Successful");
+
+            }
+        }
+        public decimal CheckAccountBalance()
+        {
+            decimal balance = 1000;
+            return balance;
+            Console.WriteLine("Pls Enter Your Account Number");
+            Console.ReadLine();
+        }
+        public string CheckBVN(string PhoneNumber)
+        {
+            string BVN = "22718794873";
+            return BVN;
+            Console.WriteLine("Pls Enter Your PhoneNumber");
+            PhoneNumber = Console.ReadLine();
+            if (PhoneNumber == "07033018078")
+            {
+                Console.WriteLine("22718794873");
+            }
+        }
+    }
+}
---
+++ Assignment2/Program.cs
@@ -0,0 +1,22 @@
+﻿using Assignment2.Models;
+using System;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Customer Shola = new Customer();
+
+            Shola.Login();
+            Shola.CheckBVN("");
+            Shola.CheckAccountBalance();
+
+
+
+           
+        }
+    }
+}
